4dd28f211417a039297145e883fea0898ff6c0d1
Nagasrivani <136495453+Nagasrivani@users.noreply.github.com>
2023-08-07 21:23:04 +0530

    Add files via upload

---
+++ chefshoes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N,M;
+    cin>>N>>M;
+    if(N<=M)
+    {
+        cout<<N<<endl;
+    }
+    else
+    {
+        cout<<(2*N-M)<<endl;
+    }
+    return 0;
+}
