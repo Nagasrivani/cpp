c0e2bc82d3e3e176bb212fcfa0e0c8cb08c9564d
Nagasrivani <136495453+Nagasrivani@users.noreply.github.com>
2023-08-10 22:00:57 +0530

    Add files via upload

---
+++ codearray.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int sample_array[5]= {5, 3, 2, 4, 1};
+    
+    // command for displaying the elements of the array
+    for(int i=0;i<5;i++)
+    {
+        cout<<sample_array[i];
+    }
+    cout<<endl;
+
+    // output specific index from the array
+    cout<<sample_array[3];
+}
